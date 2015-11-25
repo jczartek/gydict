0c44f12f667f8baca1499761e3271d8db31c6ac7
Jakub Czartek <kuba@linux.pl>
2015-11-25 17:28:02 +0000

    Settings: Added font-name setting.

--- src/gy-preferences-page-general.c
+++ src/gy-preferences-page-general.c
@@ -21,7 +21,10 @@
 
 struct _GyPreferencesPageGeneral
 {
-  GyPreferencesPage parent;
+  GyPreferencesPage  parent;
+
+  GtkFontButton     *font_button;
+  GSettings         *settings;
 };
 
 G_DEFINE_TYPE (GyPreferencesPageGeneral, gy_preferences_page_general, GY_TYPE_PREFERENCES_PAGE)
@@ -72,16 +75,34 @@ gy_preferences_page_general_set_property (GObject      *object,
 }
 
 static void
+gy_preferences_page_general_constructed (GObject *object)
+{
+  GyPreferencesPageGeneral *self = GY_PREFERENCES_PAGE_GENERAL (object);
+
+  g_return_if_fail (GY_IS_PREFERENCES_PAGE_GENERAL (self));
+
+  G_OBJECT_CLASS (gy_preferences_page_general_parent_class)->constructed;
+
+  g_settings_bind (self->settings,    "font-name",
+                   self->font_button, "font-name",
+                   G_SETTINGS_BIND_DEFAULT);
+
+}
+
+static void
 gy_preferences_page_general_class_init (GyPreferencesPageGeneralClass *klass)
 {
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
+  object_class->constructed = gy_preferences_page_general_constructed;
   object_class->finalize = gy_preferences_page_general_finalize;
   object_class->get_property = gy_preferences_page_general_get_property;
   object_class->set_property = gy_preferences_page_general_set_property;
 
   gtk_widget_class_set_template_from_resource (widget_class, "/org/gtk/gydict/gy-preferences-page-general.ui");
+  gtk_widget_class_bind_template_child (widget_class, GyPreferencesPageGeneral, font_button);
+  gtk_widget_class_bind_template_child (widget_class, GyPreferencesPageGeneral, settings);
 }
 
 static void
